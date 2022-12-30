96cd3789668074c6dff71c58b2f9973f27cff13d
IoannisGkanatsios <ioannisgkan259@gmail.com>
2022-12-30 18:10:31 +0000

    square cosine angle correction was added

--- src/angle_correction.py
+++ src/angle_correction.py
@@ -4,8 +4,9 @@ This script is used to perform incidence angle correction on Sentinel-1 data
 usage: angle_correction.py [-h] 
                            [-o OUTDIR] 
                            [-i LOAD_SAR] 
-                           [--vector VECTOR] 
                            [--ref_angle REF_ANGLE]
+                           [--linear LINEAR] 
+                           [--sqr_cosine]
 
 optional arguments:
   -h, --help            show this help message and exit
@@ -13,26 +14,37 @@ optional arguments:
                         Specify an output directory
   -i LOAD_SAR, --load_sar LOAD_SAR
                         Provide a path to the sar data
-  --vector VECTOR       Provide a path to the vector file (shapefile)
   --ref_angle REF_ANGLE
                         Provide a value for incidence angle normalziation
+  --linear LINEAR       Performs angle correction based on linear regression. It requires a shapefile (points) as an input. 
+                        Point data should be colected from near to far range
+  --sqr_cosine          Performs angle correction based on the square cosine
                         
                         
 Example:
 
-Incidence angel correction based on the default value (33)
-------------------------------------------------------------
+Incidence angel correction using linear regression with a default value (33)
+-----------------------------------------------------------------------
 python3 angle_correction.py -i raw/subset_S1A_IW_GRDH_20210105_ice.tif 
                             -o output/sar_normalized_sandard.tif  
-                            --vector vector/water_subset.shp
+                            --linear vector/water_subset.shp
                             
                             
-Incidence angel correction at 40 degrees 
+Incidence angle correction using linear regression at 40 degrees 
 ------------------------------------------------------------
 python3 angle_correction.py -i raw/subset_S1A_IW_GRDH_20210105_ice.tif 
                             -o output/sar_normalized_sandard.tif  
                             --ref_angle 40
-                            --vector vector/water_subset.shp
+                            --linear vector/water_subset.shp
+                            
+Incidence angle correction using square cosine at 40 degrees 
+------------------------------------------------------------
+python3 angle_correction.py -i raw/subset_S1A_IW_GRDH_20210105_ice.tif 
+                            -o output/sar_normalized_sandard.tif  
+                            --sqr_cosine
+                            --ref_angle 40
+         
+                            
 '''
 
 
@@ -67,6 +79,11 @@ def read(path):
         sigma_vh = rasters[1]
         angle = rasters[2]
         profile = src.profile
+        if profile['count'] != 3:
+            print (f"It seems that the number of bands in the data is {profile['count']}" '\n' 
+            "The SAR product must contain 3 bands in the following order. 1) co-polarized band, 2)cross-polarized band, 3)incidence angle," '\n'
+            "Bands in the SAR product not in that order might produces unexpected resuls"
+    )
     return sigma_vv, sigma_vh, angle, profile
 
 
@@ -134,7 +151,7 @@ def angle_stats(gdf, raster, profile):
     return angle_allvals 
 
 
-def angle_correction_coPol(gdf, co_db, angle, profile, ref_angle=33):
+def linear_angle_correction(gdf, co_db, angle, profile, ref_angle=33):
     stats_sigma = sigma_stats(gdf, co_db, profile)
     stats_angle = angle_stats(gdf, angle, profile)
     polyval, slope = regress(stats_sigma, stats_angle)
@@ -142,25 +159,17 @@ def angle_correction_coPol(gdf, co_db, angle, profile, ref_angle=33):
     co_db_corrected = co_db - slope * (angle - ref_angle)
     return co_db_corrected
 
-def angle_correction_crossPol(gdf, cross_db, angle, profile, ref_angle=33):
-    stats_sigma = sigma_stats(gdf, cross_db, profile)
-    stats_angle = angle_stats(gdf, angle, profile)
-    polyval, slope = regress(stats_sigma, stats_angle)
-    
-    cross_db_corrected = cross_db - slope * (angle - ref_angle)
-    return cross_db_corrected
+
+def square_cosine_angle_correction(sigma_vv, angle, ref_angle=33):
+
+    sigma_nought_vv = sigma_vv * np.cos(np.deg2rad(angle))**2
+    sigma_corr_vv = (sigma_nought_vv * np.cos(np.deg2rad(ref_angle))**2) / np.cos(np.deg2rad(angle))**2
+    return sigma_corr_vv
 
 def stack(coPol, crossPol, angle):
     stacked = np.stack([coPol, crossPol, angle])
     return stacked
 
-def plot(raster_extract, angle_extract, poly_val):
-    plt.plot(angle_extract, raster_extract, 'o')
-    plt.plot(angle_extract, poly_val, color='g', lw=3)
-    plt.xlabel('Incident angle')
-    plt.ylabel('Sigma0[dB]')
-    plt.show()
-    return
 
 if __name__ == "__main__":
     print('Incidence angle normalization...', '\n',)
@@ -182,18 +191,28 @@ if __name__ == "__main__":
         help='Provide a path to the sar data'
     )
     
-    parser.add_argument(
-        '--vector',
-        type=str,
-        required=False,
-        help='Provide a path to the vector file (shapefile)'
-    )
-    
+
     parser.add_argument(
         '--ref_angle',
         type=int,
         required=False,
-        help='Provide a value for incidence angle normalziation'
+        help='Provide a value to be used for incidence angle normalziation'
+    )
+    
+    parser.add_argument(
+        '--linear',
+        type=str,
+        required=False,
+        help='Performs angle correction based on linear regression. \
+            It requires a shapefile (points) as an input. Point data should be colected \
+            from near to far range'
+    )
+    
+    parser.add_argument(
+        '--sqr_cosine',
+        action='store_true',
+        required=False,
+        help='Performs angle correction based on the square cosine'
     )
     
     args = parser.parse_args()
@@ -201,24 +220,40 @@ if __name__ == "__main__":
     
     if not args.outdir:
         parser.error('Please provide an output path (use option -o)')
-
+        
     # Load SAR data
     coPol_db, crossPol_db, angle, profile = read(args.load_sar)
     
-    # load shapefile
-    gdf = gpd.read_file(args.vector)
-    
-    if args.ref_angle:
-        # perform angle correction for both col polarized and cross polarized bands
-        coPol_corrected = angle_correction_coPol(gdf, coPol_db, angle, profile, args.ref_angle)
-        crossPol_corrected = angle_correction_coPol(gdf, crossPol_db, angle, profile, args.ref_angle)
-        stacked = stack(coPol_corrected, crossPol_corrected, angle)
-    else:
-        # perform angle correction for both col polarized and cross polarized bands
-        coPol_corrected = angle_correction_coPol(gdf, coPol_db, angle, profile)
-        crossPol_corrected = angle_correction_coPol(gdf, crossPol_db, angle, profile)
-        stacked = stack(coPol_corrected, crossPol_corrected, angle)
+    if args.linear:
+        # load shapefile
+        gdf = gpd.read_file(args.linear)
         
+        if args.ref_angle:
+            # perform angle correction specifying the angle
+            coPol_corrected = linear_angle_correction(gdf, coPol_db, angle, profile, args.ref_angle)
+            crossPol_corrected = linear_angle_correction(gdf, crossPol_db, angle, profile, args.ref_angle)
+            stacked = stack(coPol_corrected, crossPol_corrected, angle)
+            
+        elif not args.ref_angle:        
+            # perform angle correction using the defoult value of 33 degrees
+            coPol_corrected = linear_angle_correction(gdf, coPol_db, angle, profile)
+            crossPol_corrected = linear_angle_correction(gdf, crossPol_db, angle, profile)
+            stacked = stack(coPol_corrected, crossPol_corrected, angle)
+            
+    if args.sqr_cosine:
+        if args.ref_angle:
+            # perform angle correction specifying the angle
+            coPol_corrected = square_cosine_angle_correction(coPol_db, angle, args.ref_angle)
+            crossPol_corrected = square_cosine_angle_correction(crossPol_db, angle, args.ref_angle)
+            stacked = stack(coPol_corrected, crossPol_corrected, angle)
+            
+        elif not args.ref_angle:
+            # perform angle correction specifying the angle
+            coPol_corrected = square_cosine_angle_correction(coPol_db, angle)
+            crossPol_corrected = square_cosine_angle_correction(crossPol_db, angle)
+            stacked = stack(coPol_corrected, crossPol_corrected, angle)
+            
+    
     # write 
     write(stacked, profile, args.outdir)
     print ('DONE')
